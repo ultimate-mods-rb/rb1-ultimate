34fd41f6e3e4a4e09f14f4dc901e142887909128
Synchrony <113392466+iamSynchrony@users.noreply.github.com>
2026-03-29 01:38:23 -0400

    python build scripts (#5)
    * python build scripts
    
    this took like a few hours to write but much faster than the prev garbage so i'm very proud.
    
    * use python in actions?
    
    i have serious doubts that i'm not completely missing something

---
+++ _build_ps3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import dependencies.buildark as buildark
+
+buildark.build_ark(
+	["./_ark"],
+	"./_build/ps3/USRDIR/gen/",
+	"./temp_ark_ps3",
+	"patch_ps3",
+	"rb1_patch",
+	[
+		r".*\.bik",
+		r".*\..*_wii",
+		r".*\.xbvwii",
+		r".*\..*_xbox",
+		r".*_out.*",
+		r".*_dbg\.milo.*",
+		r".*_rt\.milo.*",
+		r".*\.bak",
+		r".*\.png",
+		r".*\.jpg",
+		r".*\.dds",
+		r".*\.xcf",
+		r".*\.sh",
+		r".*\.py"
+	]
+)
